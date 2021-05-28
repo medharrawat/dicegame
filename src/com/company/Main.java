2875a809e4bd109ba61b2cb7d22b10697231fbf4
audrey457902 <84944146+audrey457902@users.noreply.github.com>
2021-05-27 20:04:33 -0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,44 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    Random r = new Random();
-	    int [] p1 = randFirst();
-    }
+	public static void main(String[] args) {
+		int [] player = roll();
+		int [] comp = roll();
+		int playerTotal = total(player);
+		int compTotal = total(comp);
+		String winner = compare (playerTotal, compTotal);
+		//System.out.println(Arrays.toString(player));
+		//System.out.println(Arrays.toString(comp));
+		//System.out.println("Player's total: " + playerTotal);
+        	//System.out.println("Computer's total: " + compTotal);
+        	//System.out.println(winner + " wins!");
+	}
 	
-	public static int[] randFirst(){
-        int [] arr = new int [5];
-        for (int i = 0; i >= arr.length; i++){
-            
-        }
-        return arr;
+	public static int [] roll(){
+		Random r = new Random();
+        	int [] arr = new int [5];
+        	for (int i = 0; i < arr.length; i++){
+            		arr [i] = r.nextInt(6) + 1;
+        	}
+        	return arr;
 	//hello
-    }
+    	}
+	
+	public static int total (int [] arr){
+		int temp = 0;
+		for (int j = 0; j < arr.length; j++){
+			temp += arr [j];
+		}
+		return temp;
+	}
+	
+	public static String compare (int p, int c){
+		if (p > c){
+			return "Player";
+		} else if (c > p){
+			return "Computer";
+		} else {
+			return "Tie";
+		}
+	}
 }
