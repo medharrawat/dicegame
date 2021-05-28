caaa5e91c4614161e4590267ac8ca789540f22d9
audrey457902 <84944146+audrey457902@users.noreply.github.com>
2021-05-27 17:23:14 -0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int [] p1 = randFirst();
+    }
+	
+	public static int[] randFirst(){
+        int [] arr = new int [5];
+        for (int i = 0; i >= arr.length; i++){
+            
+        }
+        return arr;
     }
 }
